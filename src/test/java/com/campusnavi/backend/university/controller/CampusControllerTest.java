805eace2f3bc9b69820cb035c1e8460ffe773c99
Duk-Hub <myyds0723@gmail.com>
2026-04-10 20:17:21 +0900

    refactor: 테스트코드 메서드명 변경 및 Displayname 추가 (#25)

--- src/test/java/com/campusnavi/backend/university/controller/CampusControllerTest.java
+++ src/test/java/com/campusnavi/backend/university/controller/CampusControllerTest.java
@@ -6,6 +6,7 @@ import com.campusnavi.backend.support.ControllerSliceTest;
 import com.campusnavi.backend.university.dto.CampusSummaryResponse;
 import com.campusnavi.backend.university.dto.DepartmentSummaryResponse;
 import com.campusnavi.backend.university.service.CampusService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -28,7 +29,8 @@ class CampusControllerTest {
     private CampusService campusService;
 
     @Test
-    void getCampusList_조회성공_200반환() throws Exception {
+    @DisplayName("캠퍼스 조회에 성공하면 DTO와 함께 200반환")
+    void getCampusListSuccess() throws Exception {
         //given
         CampusSummaryResponse response = new CampusSummaryResponse(1L,"서울캠퍼스");
         given(campusService.getCampusList()).willReturn(List.of(response));
@@ -41,7 +43,8 @@ class CampusControllerTest {
     }
 
     @Test
-    void getDepartmentList_조회성공_200반환() throws Exception{
+    @DisplayName("학과 조회에 성공하면 DTO와 함께 200반환")
+    void getDepartmentListSuccess() throws Exception{
         //given
         DepartmentSummaryResponse response = new DepartmentSummaryResponse(1L, "컴퓨터공학과");
         given(campusService.getDepartmentList(1L)).willReturn(List.of(response));
@@ -54,7 +57,8 @@ class CampusControllerTest {
     }
 
     @Test
-    void getDepartmentList_잘못된PathVariable타입_400반환() throws Exception {
+    @DisplayName("잘못된 형식의 CampusId를 보내면 400반환")
+    void getDepartmentListInvalidParam() throws Exception {
         mockMvc.perform(get("/api/v1/campuses/abc/departments"))
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.success").value(false))
@@ -62,7 +66,8 @@ class CampusControllerTest {
     }
 
     @Test
-    void getDepartmentList_존재하지않는캠퍼스ID_404반환() throws Exception {
+    @DisplayName("존재하지 않는 CampusId를 보내면 404반환")
+    void getDepartmentListNotFoundByCampusId() throws Exception {
         //given
         Long campusId = 99L;
         given(campusService.getDepartmentList(campusId))
