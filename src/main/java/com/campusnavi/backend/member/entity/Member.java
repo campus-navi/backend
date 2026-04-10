4208ca73208c88c7c7e0f608ebbf2c5ba43da18a
DongUk <myyds0723@gmail.com>
2026-04-10 11:05:31 +0900

    feat: Member 엔티티 추가

---
+++ src/main/java/com/campusnavi/backend/member/entity/Member.java
@@ -0,0 +1,77 @@
+package com.campusnavi.backend.member.entity;
+
+import com.campusnavi.backend.global.common.BaseEntity;
+import com.campusnavi.backend.university.entity.Campus;
+import com.campusnavi.backend.university.entity.Department;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SoftDelete;
+import org.hibernate.annotations.SoftDeleteType;
+
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SoftDelete(strategy = SoftDeleteType.TIMESTAMP, columnName = "deleted_at")
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false,unique = true)
+    private String email;
+
+    @Column(nullable = false, length = 30,unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    @Column(nullable = false)
+    private Long universityId;
+
+    @ManyToOne
+    @JoinColumn(name = "campus_id",nullable = false)
+    private Campus campus;
+
+    @Column(nullable = false)
+    private Integer admissionYear;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private MemberRole role;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private MemberStatus status;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.PERSIST)
+    private List<MemberDepartment> memberDepartments;
+
+    public static Member join(String email, String username, String password, String nickname,
+                              Long universityId, Campus campus, Integer admissionYear) {
+        Member member = new Member();
+        member.email = email;
+        member.username = username;
+        member.password = password;
+        member.nickname = nickname;
+        member.universityId = universityId;
+        member.campus = campus;
+        member.admissionYear = admissionYear;
+        member.role = MemberRole.USER;
+        member.status = MemberStatus.ACTIVE;
+        return member;
+    }
+
+    public void addDepartment(Department department){
+        MemberDepartment memberDepartment = MemberDepartment.create(this,department);
+        memberDepartments.add(memberDepartment);
+    }
+}
