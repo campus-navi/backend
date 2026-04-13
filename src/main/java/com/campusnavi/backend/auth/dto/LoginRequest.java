ac4243231c4441d1d1a065b70813375218e7ca25
DongUk <myyds0723@gmail.com>
2026-04-13 19:49:18 +0900

    feat: 로그인관련 DTO, 에러코드, 쿼리메서드 추가

---
+++ src/main/java/com/campusnavi/backend/auth/dto/LoginRequest.java
@@ -0,0 +1,12 @@
+package com.campusnavi.backend.auth.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginRequest(
+        @NotBlank
+        String username,
+
+        @NotBlank
+        String password
+) {
+}
--- src/main/java/com/campusnavi/backend/member/repository/MemberRepository.java
+++ src/main/java/com/campusnavi/backend/member/repository/MemberRepository.java
@@ -3,8 +3,11 @@ package com.campusnavi.backend.member.repository;
 import com.campusnavi.backend.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member,Long> {
     boolean existsByEmail(String email);
     boolean existsByUsername(String username);
     boolean existsByNickname(String nickname);
+    Optional<Member> findByUsername(String username);
 }
