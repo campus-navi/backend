5f25875d52a6c794203eb8c8d564e62eab702ecc
DongUk <myyds0723@gmail.com>
2026-04-13 16:45:21 +0900

    feat: 회원가입 관련 ErrorCode, DTO 추가 및 쿼리 추가

---
+++ src/main/java/com/campusnavi/backend/auth/dto/SignUpRequest.java
@@ -0,0 +1,28 @@
+package com.campusnavi.backend.auth.dto;
+
+import jakarta.validation.constraints.*;
+
+public record SignUpRequest(
+        @NotBlank
+        String verifiedToken,
+
+        @NotBlank
+        @Size(min = 5, max = 30, message = "아이디는 5~30자 이내로 입력해주세요.")
+        @Pattern(regexp = "^[a-z0-9_]+$", message = "영문 소문자, 숫자, 밑줄(_)만 사용 가능합니다.")
+        String username,
+
+        @NotBlank
+        @Size(min = 8, max = 16, message = "비밀번호는 8~16자 이내로 입력해주세요.")
+        @Pattern(regexp = "^[a-zA-Z\\d!@#$%^&*()]{8,16}$", message = "8~16자의 영문,숫자,특수문자를 사용해 주세요.")
+        String password,
+
+        @NotBlank
+        String nickname,
+
+        @NotNull
+        Long departmentId,
+
+        @NotNull
+        Integer admissionYear
+) {
+}
