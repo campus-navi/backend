905a42a4159d05cfbf765ba25307c8aab2c3d94e
DongUk <myyds0723@gmail.com>
2026-04-14 16:05:47 +0900

    fix: 엔티티 누락 unique 제약 추가

--- src/main/java/com/campusnavi/backend/university/entity/University.java
+++ src/main/java/com/campusnavi/backend/university/entity/University.java
@@ -14,7 +14,7 @@ public class University {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String name;
 
     public static University create(String name) {
