420a451efc65758410a6ec84bccb74a745a9cc4d
DongUk <myyds0723@gmail.com>
2026-04-17 17:31:26 +0900

    refactor: migration파일 정렬

--- src/main/resources/db/migration/V11__create_post_like_scrap.sql
+++ src/main/resources/db/migration/V11__create_post_like_scrap.sql
@@ -1,21 +1,23 @@
 -- PostLike 테이블 생성
-CREATE TABLE post_like (
-    id         BIGSERIAL PRIMARY KEY,
-    member_id  BIGINT NOT NULL,
-    post_id    BIGINT NOT NULL,
+CREATE TABLE post_like
+(
+    id        BIGSERIAL PRIMARY KEY,
+    member_id BIGINT NOT NULL,
+    post_id   BIGINT NOT NULL,
     CONSTRAINT uq_post_like UNIQUE (member_id, post_id),
     CONSTRAINT fk_post_like_member FOREIGN KEY (member_id) REFERENCES member (id),
-    CONSTRAINT fk_post_like_post   FOREIGN KEY (post_id)   REFERENCES post (id)
+    CONSTRAINT fk_post_like_post FOREIGN KEY (post_id) REFERENCES post (id)
 );
 
 -- PostScrap 테이블 생성
-CREATE TABLE post_scrap (
-                            id         BIGSERIAL PRIMARY KEY,
-                            member_id  BIGINT NOT NULL,
-                            post_id    BIGINT NOT NULL,
-                            CONSTRAINT uq_post_scrap UNIQUE (member_id, post_id),
-                            CONSTRAINT fk_post_scrap_member FOREIGN KEY (member_id) REFERENCES member (id),
-                            CONSTRAINT fk_post_scrap_post   FOREIGN KEY (post_id)   REFERENCES post (id)
+CREATE TABLE post_scrap
+(
+    id        BIGSERIAL PRIMARY KEY,
+    member_id BIGINT NOT NULL,
+    post_id   BIGINT NOT NULL,
+    CONSTRAINT uq_post_scrap UNIQUE (member_id, post_id),
+    CONSTRAINT fk_post_scrap_member FOREIGN KEY (member_id) REFERENCES member (id),
+    CONSTRAINT fk_post_scrap_post FOREIGN KEY (post_id) REFERENCES post (id)
 );
 
 CREATE INDEX idx_post_like_member_post ON post_like (member_id, post_id);
