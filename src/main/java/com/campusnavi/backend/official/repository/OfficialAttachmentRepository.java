2dd203b78a8f3f567b716dcf29ce8e946713856f
Duk-Hub <myyds0723@gmail.com>
2026-04-20 17:34:58 +0900

    Revert "feat: 공식정보 도메인 추가 (#95)" (#96)
    This reverts commit 96328c0192d92562f3c648db25e203adab7a1009.

--- src/main/java/com/campusnavi/backend/official/repository/OfficialAttachmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.campusnavi.backend.official.repository;
-
-import com.campusnavi.backend.official.entity.OfficialAttachment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OfficialAttachmentRepository extends JpaRepository<OfficialAttachment, Long> {
-}
