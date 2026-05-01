4e14a3cee9801fe8083d0bd06633a4ff15666a02
Duk-Hub <myyds0723@gmail.com>
2026-05-01 18:29:43 +0900

    fix: Swagger SecurityRequirement 불일치 수정 (#138)

--- src/main/java/com/campusnavi/backend/university/controller/CampusController.java
+++ src/main/java/com/campusnavi/backend/university/controller/CampusController.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirements;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +24,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/campuses")
+@SecurityRequirements({})
 public class CampusController {
 
     private final CampusService campusService;
