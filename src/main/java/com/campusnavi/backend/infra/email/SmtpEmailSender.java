af83f688e93087a44180cd75f3cc9aaa8321053f
DongUk <myyds0723@gmail.com>
2026-05-11 16:45:35 +0900

    fix: 누락된 UnsupportedEncodingException 처리 추가

--- src/main/java/com/campusnavi/backend/infra/email/SmtpEmailSender.java
+++ src/main/java/com/campusnavi/backend/infra/email/SmtpEmailSender.java
@@ -10,6 +10,7 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
+import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 
 @Component
@@ -29,7 +30,7 @@ public class SmtpEmailSender implements EmailSender{
             helper.setSubject(subject);
             helper.setText(content, true);
             mailSender.send(message);
-        } catch (MessagingException e) {
+        } catch (MessagingException | UnsupportedEncodingException e) {
             throw new BusinessException(ErrorCode.EMAIL_SEND_FAIL);
         }
     }
